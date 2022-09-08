f0c5f68b44bbea14c1fb8b9cea651df72f80859a
Adrellan <2001adrian.fekete@gmail.com>
2022-09-08 11:11:41 +0200

    Orai_munka

---
+++ Orak/ora1.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+from cgi import print_directory
+import sys
+
+def nevkiir(nev):
+    print("Hali "+nev)
+
+def main():
+    a = "csumi"
+    #print(a)
+    #print(sys.argv)
+    nevkiir("Adrian")
+
+if __name__ == "__main__":
+    main()
